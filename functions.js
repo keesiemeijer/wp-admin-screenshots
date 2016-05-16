c527e4fa7afdce6eecf42b6f36ab608a5092a959
keesiemeijer <keesie.meijer@gmail.com>
2016-05-16 21:29:59 +0200

    Add loop to functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
-var require = patchRequire( require );
+var require = patchRequire( require ),
+	utils = require( 'utils' );
 
 function _sanitize_filename( filename ) {
 	filename = filename.toLowerCase();
@@ -7,6 +8,7 @@ function _sanitize_filename( filename ) {
 
 	return filename;
 }
+
 
 function _toggle_screen_options( casper ) {
 
@@ -36,8 +38,7 @@ function _toggle_screen_options( casper ) {
 	}
 };
 
-
-exports.take_screenshot = function( file, dimensions, casper ) {
+function _take_screenshot( file, dimensions, casper) {
 
 	var filename = _sanitize_filename( file );
 	var dir = casper.options[ 'screenshot_options' ][ 'save_dir' ];
@@ -50,6 +51,31 @@ exports.take_screenshot = function( file, dimensions, casper ) {
 	} else {
 		casper.echo( 'Could not take screenshot. Directory parameter missing.' );
 	}
+}
+
+
+exports.get_menu_items = function( css_selector, casper ) {
+	links = casper.evaluate( function( css_selector ) {
+		var elements = __utils__.findAll( css_selector );
+
+		elements = elements.map( function( e ) {
+			var href = e.getAttribute( 'href' );
+			if ( 0 !== href.indexOf( "customize.php" ) ) {
+				return href;
+			}
+		} );
+
+		return elements.filter( function( item ) {
+			return typeof item === 'string';
+		} );
+	}, css_selector );
+
+	return utils.unique( links );;
+}
+
+
+exports.take_screenshot = function( file, dimensions, casper ) {
+  _take_screenshot( file, dimensions, casper);	
 };
 
 
@@ -76,7 +102,71 @@ exports.check_screen_options_checkboxes = function( casper ) {
 	}
 };
 
+exports.loop = function( url, links, options, casper ) {
+	casper.each( links, function( self, link, i ) {
 
+		casper.thenOpen( url + '/wp-admin/' + link, function() {
+
+			casper.emit( 'after.wp_admin_open', link );
+
+			//set the viewport to the desired height and width
+			casper.viewport( options[ 'viewport-width' ], 400 );
+
+			casper.echo( "\nOpening " + url + '/wp-admin/' + link + ' at ' + options[ 'viewport-width' ] + 'px' );
+
+			// wait for stuff to load (like in customizer)
+			casper.then( function() {
+				casper.wait( 2000 );
+			} );
+
+			casper.then( function() {
+				if ( this.exists( '#show-settings-link' ) && options[ 'check-screen-options' ] ) {
+					functions.check_screen_options_checkboxes( casper );
+				}
+			} );
+
+
+			casper.then( function() {
+
+				if ( !options[ "show-screen-options" ] ) {
+					if ( this.visible( '#screen-options-wrap' ) ) {
+						functions.toggle_screen_options( casper );
+					}
+				} else {
+					if ( !this.visible( '#screen-options-wrap' ) ) {
+						functions.toggle_screen_options( casper );
+					}
+				}
+			} );
+
+
+			casper.then( function() {
+				casper.scrollTo( 0, 0 );
+
+				documentHeight = casper.evaluate( function() {
+					return __utils__.getDocumentHeight();
+				} );
+
+				var dimensions = {
+					top: 0,
+					left: 0,
+					width: options[ 'viewport-width' ],
+					height: documentHeight
+				}
+
+				//dimensions = filters.get_dimensions( link, dimensions );
+
+				_take_screenshot( link, dimensions, casper )
+
+				casper.emit( 'after.screenshot', link );
+			} );
+
+			casper.then( function() {
+				casper.wait( 2000 );
+			} );
+		} );
+	} );
+};
 
 exports.toggle_screen_options = function( casper ) {
 	_toggle_screen_options( casper );
--- wp-admin-screenshots.js
+++ wp-admin-screenshots.js
@@ -1,9 +1,13 @@
 var utils = require( 'utils' ),
-	casper = require( 'casper' ).create(),
+	casper = require( 'casper' ).create( {
+		// verbose: true,
+		// logLevel: "debug"
+	} ),
 	functions = require( './functions' ),
 	filters = require( './filters' ),
-	url = '',
+	base_url = '',
 	admin_links = [],
+	submenu_links = [],
 	defaults = {
 		'admin_user': 'admin',
 		'admin_password': 'password',
@@ -17,8 +21,8 @@ var utils = require( 'utils' ),
 if ( casper.cli.args.length === 0 ) {
 	casper.echo( "Please provide an url" );
 } else {
-	url = casper.cli.args[ 0 ];
-	defaults[ 'save_dir' ] = url.replace( /\W/g, '-' ).replace( /^https?-+/, '' );
+	base_url = casper.cli.args[ 0 ];
+	defaults[ 'save_dir' ] = base_url.replace( /\W/g, '-' ).replace( /^https?-+/, '' );
 }
 
 var options = utils.mergeObjects( defaults, casper.cli.options );
@@ -31,18 +35,18 @@ casper.options[ 'screenshot_options' ] = options;
 
 // utils.dump( options );
 
-// casper.on('remote.message', function(msg) {
-//     this.echo('remote message caught: ' + msg);
-// });
+// casper.on( 'remote.message', function( msg ) {
+// 	this.echo( 'remote message caught: ' + msg );
+// } );
 
 filters.instance( casper );
 
 // Open /wp-login.php
-if ( url.length ) {
+if ( base_url.length ) {
 	// start casper and check if /wp-login.php exists
-	casper.start( url + "/wp-login.php", function() {
+	casper.start( base_url + "/wp-login.php", function() {
 		if ( this.status().currentHTTPStatus !== 200 ) {
-			this.echo( 'Could not open: ' + url + "/wp-login.php" ).exit();
+			this.echo( 'Could not open: ' + base_url + "/wp-login.php" ).exit();
 		}
 	} );
 }
@@ -52,7 +56,7 @@ if ( url.length ) {
 casper.then( function() {
 	if ( this.exists( '#loginform' ) ) {
 
-		this.echo( "logging in at " + url + "/wp-login.php" )
+		this.echo( "logging in at " + base_url + "/wp-login.php" )
 		this.fillSelectors( '#loginform', {
 			'#user_login': options[ 'admin_user' ],
 			'#user_pass': options[ 'admin_password' ]
@@ -71,25 +75,22 @@ casper.then( function() {
 
 // Get admin pages from admin menu
 casper.then( function() {
-	links = this.evaluate( function() {
-		var elements = __utils__.findAll( '#adminmenu a' );
 
-		elements = elements.map( function( e ) {
-			var href = e.getAttribute( 'href' );
-			if ( 0 !== href.indexOf( "customize.php" ) ) {
-				return href;
-			}
-		} );
+	var top_level = '';
+	admin_links = functions.get_menu_items( '#adminmenu a', casper );
 
-		return elements.filter( function( item ) {
-			return typeof item === 'string';
-		} );
-	} );
+	if ( this.exists( '#sidemenu' ) ) {
+		var sidemenu = functions.get_menu_items( '#sidemenu a', casper );
+		if ( sidemenu.length ) {
+			top_level = ' top level';
+			admin_links = admin_links.concat( sidemenu );
+		}
+	}
 
-	admin_links = utils.unique( links );
+	admin_links = utils.unique( admin_links );
 
 	if ( admin_links.length ) {
-		this.echo( admin_links.length + " admin links found\n" );
+		this.echo( admin_links.length + top_level + " admin links found\n" );
 	} else {
 		this.echo( "No admin links found" ).exit();
 	}
@@ -98,68 +99,7 @@ casper.then( function() {
 
 //Loop through admin pages and take a screenshot
 casper.then( function() {
-
-	casper.each( admin_links, function( self, link, i ) {
-
-		casper.thenOpen( url + '/wp-admin/' + link, function() {
-
-			//set the viewport to the desired height and width
-			this.viewport( options[ 'viewport-width' ], 400 );
-
-			casper.echo( "\nOpening " + url + '/wp-admin/' + link + ' at ' + options[ 'viewport-width' ] + 'px' );
-
-			// wait for stuff to load (like in customizer)
-			casper.then( function() {
-				casper.wait( 2000 );
-			} );
-
-			casper.then( function() {
-				if ( this.exists( '#show-settings-link' ) && options[ 'check-screen-options' ] ) {
-					functions.check_screen_options_checkboxes( casper );
-				}
-			} );
-
-
-			casper.then( function() {
-
-				if ( !options[ "show-screen-options" ] ) {
-					if ( this.visible( '#screen-options-wrap' ) ) {
-						functions.toggle_screen_options( casper );
-					}
-				} else {
-					if ( !this.visible( '#screen-options-wrap' ) ) {
-						functions.toggle_screen_options( casper );
-					}
-				}
-			} );
-
-
-			casper.then( function() {
-				casper.scrollTo( 0, 0 );
-
-				documentHeight = casper.evaluate( function() {
-					return __utils__.getDocumentHeight();
-				} );
-
-				var dimensions = {
-					top: 0,
-					left: 0,
-					width: options[ 'viewport-width' ],
-					height: documentHeight
-				}
-
-				dimensions = filters.get_dimensions( link, dimensions );
-
-				functions.take_screenshot( link, dimensions, casper )
-
-				casper.emit( 'after.screenshot', link );
-			} );
-
-			casper.then( function() {
-				casper.wait( 2000 );
-			} );
-		} );
-	} );
+	functions.loop( base_url, admin_links, options, casper);
 } );
 
 
