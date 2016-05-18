5928ebaa690557ca4e1abd9efbccbb002b00a56e
keesiemeijer <keesie.meijer@gmail.com>
2016-05-18 22:07:01 +0200

    Add get_submenu_links action

--- do-events.js
+++ do-events.js
@@ -18,16 +18,54 @@ function do_events() {
 	// Filter admin links. Use this to set the admin links to take screenshots for.
 	// casper.on( 'wp_admin_links', set_admin_links );
 
+	// Get submenu links (WP < 2.7)
+	casper.on( 'open_wp_admin_link', get_submenu_links );
+
+	// remove get_submenu_links event listner
+	casper.on( 'after_wp_admin_loop', remove_get_submenu_links_action );
+
 	// Check screen options after opening page
-	casper.on( 'after.open_wp_admin_link', check_screen_options );
+	casper.on( 'open_wp_admin_link', check_screen_options );
 
 	// Opens closed meta boxes
-	casper.on( 'after.open_wp_admin_link', open_meta_boxes );
+	casper.on( 'open_wp_admin_link', open_meta_boxes );
 }
 
 
 var set_admin_links = function() {
 	casper.options[ 'wp_admin_links' ] = [ 'post-new.php' ];
+}
+
+var get_submenu_links = function( parent_link ) {
+
+	// parent links
+	var parent_links = casper.options[ 'wp_admin_links' ];
+	var child_links = casper.options[ 'wp_admin_submenu_links' ];
+
+	if ( casper.exists( '#submenu' ) ) {
+
+		var admin_links = functions.get_menu_items( '#submenu a', casper );
+
+		admin_links = functions.sanitize_links( admin_links );
+
+		// Check if it's not a parent link
+		admin_links = admin_links.filter( function( link ) {
+			return ( ( parent_links.indexOf( link ) === -1 ) || ( parent_link !== link ) );
+		} );
+
+		if ( admin_links.length ) {
+			casper.echo( 'Getting sub menu item links' );
+			child_links = utils.unique( child_links.concat( admin_links ) );
+			casper.options[ 'wp_admin_submenu_links' ] = child_links;
+		}
+		//utils.dump( casper.options[ 'wp_admin_submenu_links' ] );
+	}
+}
+
+
+var remove_get_submenu_links_action = function() {
+	casper.echo( 'removing get_submenu_links action' );
+	casper.removeListener( 'open_wp_admin_link', get_submenu_links );
 }
 
 
